f09fe902f5750d51be00b2d205b8e0637098c5c7
Demelziraptor <demelza@circularvale.com>
2014-07-11 18:38:58 +0100

    adding timing

--- test2.py
+++ test2.py
@@ -1,3 +1,5 @@
+import timeit
+import sys
 
 class TestPaths():
 
@@ -11,6 +13,7 @@ class TestPaths():
 
     def path_permutations(self, iterable, r=None):
         """ generator returns all possible paths of given coordinates """
+        times['mid'].append((sys._getframe().f_code.co_name, timeit.default_timer()))
         pool = tuple(iterable)
         n = len(pool)
         r = n if r is None else r
@@ -35,6 +38,7 @@ class TestPaths():
 
     def fruit_combinations(self, items, n):
         """ generator returns possible combinations for a list and num required"""
+        times['mid'].append((sys._getframe().f_code.co_name, timeit.default_timer()))
         if n==0: yield []
         else:
             for i in xrange(len(items)):
@@ -43,6 +47,7 @@ class TestPaths():
 
     def gen_unique_fruit_combinations(self, fruit_list, current=False):
         """ generator returns unique combinations for a list containing lists of coords """
+        times['mid'].append((sys._getframe().f_code.co_name, timeit.default_timer()))
         if not current:
             if len(fruit_list) == 1:
                 for i in fruit_list[0]:
@@ -62,6 +67,7 @@ class TestPaths():
 
     def unique_fruit_combinations(self, fruit):
         """ gets possible combinations for type of fruit, then combines to create coord combinations """
+        times['mid'].append((sys._getframe().f_code.co_name, timeit.default_timer()))
         fruit_list = []
         for i in range(len(fruit)):
             current_list = []
@@ -72,6 +78,7 @@ class TestPaths():
 
     def different_paths(self):
         """ finds all possible paths and calculates minimum """
+        times['mid'].append((sys._getframe().f_code.co_name, timeit.default_timer()))
         min_distance = 0
         min_path = []
         for y in self.unique_fruit_combinations(self.coord_list):
@@ -98,12 +105,22 @@ class TestPaths():
         return min_path
         
     def calculate_dinner_location(self):
+        times['mid'].append((sys._getframe().f_code.co_name, timeit.default_timer()))
         self.dinner_location = self.different_paths()[0]
         
     def _distance(self, p0, p1):
         """ difference in x + difference in y to calculate distance """
         return abs(p0[0] - p1[0]) + abs(p0[1] - p1[1])
         
-        
-        
+
+
+times = {}
+times['START'] = timeit.default_timer()
+times['mid'] = []
 TestPaths()
+times['END'] = timeit.default_timer()
+
+for time in times['mid']:
+    print 'Function:', time[0], ' ~ time:', time[1]
+print '\n\nTotal:', str(times['END'] - times['START'])
+
